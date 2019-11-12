c26dc09210540df080f34ba057761b2e752eb057
Olcatsy <olga.filiushkina@gmail.com>
2019-11-12 16:06:48 -0500

    -added a result card that displays different result depending on the choices

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ $(function() {
 // DOM ELEMENTS VARIABLES
 
 const $startCard = $('#startCard');
-console.log($startCard);
-const $q1 = $('#question1');
-const $q2 = $('#question2');
-const $q3 = $('#question3');
-const $q4 = $('#question4');
-const $q5 = $('#question5');
+const $q1 = $('#q1');
+const $q2 = $('#q2');
+const $q3 = $('#q3');
+const $q4 = $('#q4');
+const $q5 = $('#q5');
 const $yes = $('.yes');
 const $no = $('.no');
+const $result = $('#result');
 
 
 // OTHER VARIABLES
@@ -33,15 +33,63 @@ $('#start').on('click',function() {
     switchCard($startCard, $q1);
 })
 
-// q1
-$('.yes').on('click', function() {
+// q1 - Do you consider yourself a softie?
+$('#q1 .yes').on('click', function() {
     switchCard($q1, $q2);
 }) 
 
-$('.no').on('click', function() {
+$('#q1 .no').on('click', function() {
     switchCard($q1, $q5);
 })
 
+// q2 - Are you salty?
+$('#q2 .yes').on('click', () => {
+    switchCard($q2, $q3);
+})
+
+$('#q2 .no').on('click', () => {
+    switchCard($q2, $q4);
+})
+
+// q3 - Do your friends think you are stinky?
+$('#q3 .yes').on('click', () => {
+    switchCard($q3, $result);
+    // Result: Gorgonzola
+    $result.append('<p>You are gorgonzola! A stinky boy!</p>');
+})
+
+
+$('#q3 .no').on('click', () => {
+    switchCard($q3, $result);
+    // Result: Feta
+    $result.append('<p>You are feta! Strong and delicous!</p>');
+})
+
+// q4 - Are you a crumbly mess
+$('#q4 .yes').on('click', () => {
+    switchCard($q4, $result);
+    // Result: goat cheese
+    $result.append('<p>You are goat cheese! Bheeeee! </p>')
+})
+
+$('#q4 .no').on('click', () => {
+    switchCard($q4, $result);
+    // Result: brie
+    $result.append('<p>You are brie! Good with sweet AND savoury accompaniments! </p>');
+})
+
+// q5 - Are you full of delicious smoky flavour?
+$('#q5 .yes').on('click', () => {
+    switchCard($q5, $result);
+    // Result: smoked gouda
+    $result.append('<p>You are smoked gouda! Gouda for you! </p>');
+})
+
+$('#q5 .no').on('click', () => {
+    switchCard($q5, $result);
+    // Result: manchego
+    $result.append('<p>You are manchego! A tasty native of Spain!</p>');
+})
 
 
 
