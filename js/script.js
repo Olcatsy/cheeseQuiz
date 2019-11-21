91b6abb094aa9a57a7ea09ee7c7e196e4852ec71
Olcatsy <olga.filiushkina@gmail.com>
2019-11-20 22:43:23 -0500

    -added and animated cheese icons to the Start button -added copyright

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// Things to add:
-// -back button on each card
-// -animate card transition (?)
+// Things to do:
 // -refactor selectors for buttons 
 // -refactor for object init
 
@@ -10,49 +8,31 @@ $(function() {
 
 // *** VARIABLES STORING DOM ELEMENTS ***
 
-const $startCard = $('#startCard');
-const $startButton = $('#startButton');
-const $q1 = $('#q1');
-const $q2 = $('#q2');
-const $q3 = $('#q3');
-const $q4 = $('#q4');
-const $q5 = $('#q5');
-const $result = $('#result');
-const $restart = $('#restart');
-
+    const $startCard = $('#startCard');
+    const $startButton = $('#startButton');
+    const $q1 = $('#q1');
+    const $q2 = $('#q2');
+    const $q3 = $('#q3');
+    const $q4 = $('#q4');
+    const $q5 = $('#q5');
+    const $result = $('#result');
+    const $restart = $('#restart');
 
 
 
 // *** HELPER FUNCTIONS ***
 
-// hides the curent card and shows the next one
-const switchCard = (card1, card2) => {
-    $(card1).hide();
-    $(card2).fadeIn();
-};
+    // hides the curent card and shows the next one
+    const switchCard = (card1, card2) => {
+        $(card1).hide();
+        $(card2).fadeIn();
+    };
 
 // shows the result and a corresponding image
-const showResult = (currentCard, html, imgName) => {
-    switchCard(currentCard, $result);
-    $('.result h2').after(`<div class="cheese-img"><img src="./styles/assets/${imgName}.jpg" alt=""></div>`).after(html);
-}
-
-// animating cheese
-
-// $.keyframe.define([{
-//     name: 'cheese-rotate',
-//     '0%': {'tranform': 'rotate(20deg)'},
-//     '100%': {'transform': 'rotate(20deg)'}
-// }]);
-
-// $startButton.on('mouseenter', function() {
-//     console.log("hover")
-//     $('#cheese').playKeyFrame({
-//         name: 'cheese-rotate',
-//         duration: '1s',
-//         iterationCount: 'infinite'
-//     });
-// });
+    const showResult = (currentCard, html, imgName) => {
+        switchCard(currentCard, $result);
+        $('.result h2').after(`<div class="cheese-img"><img src="./styles/assets/${imgName}.jpg" alt=""></div>`).after(html);
+    }
 
 
 
@@ -60,85 +40,72 @@ const showResult = (currentCard, html, imgName) => {
 
 // When a button is clicked, the question card switches to the next one, depending on whether 'yes' or 'no' is clicked, and then reveals a result
 
+    // Start the quiz by clicking the START button
+    $startButton.on('click',function() {
+        // goes to the QUESTION 1 - Do you consider yourself a softie?
+        switchCard($startCard, $q1);
 
+        // listens for a button click, switches cards depending on whether YES or NO is selected
+        $('#q1 button').on('click', function() {
 
-// Start the quiz by clicking the START button
-$startButton.on('click',function() {
-    // goes to the QUESTION 1 - Do you consider yourself a softie?
-    switchCard($startCard, $q1);
+            if ($(this).data("choice") === 'yes') {
+                // goes to QUESTION 2 - Are you salty?
+                switchCard($q1, $q2);
+                
+                $('#q2 button').on('click', function() {
+                    if ($(this).data('choice') === 'yes') {
+                        // goes to QUESTION 3 - Do your friends think you are stinky?
+                        switchCard($q2, $q3);
 
-    // listens for a button click, switches cards depending on whether YES or NO is selected
-    $('#q1 button').on('click', function() {
+                        $('#q3 button').on('click', function() {
+                            if($(this).data('choice') === 'yes') {
+                                // RESULT: Gorgonzola
+                                showResult($q3, '<p>You are gorgonzola! A stinky boy!</p>', 'Gorgonzola');
 
-        if ($(this).data("choice") === 'yes') {
-            // goes to QUESTION 2 - Are you salty?
-            switchCard($q1, $q2);
-            
-            $('#q2 button').on('click', function() {
-                if ($(this).data('choice') === 'yes') {
-                    // goes to QUESTION 3 - Do your friends think you are stinky?
-                    switchCard($q2, $q3);
+                            } else {
+                                // RESULT: Feta
+                                showResult($q3, '<p>You are feta! Strong and delicous!</p>', 'Feta');
+                            };
+                        })
+                        
+                    } else {
+                        // goest to QUESTION 4 - Are you a crumbly mess?
+                        switchCard($q2, $q4);
 
-                    $('#q3 button').on('click', function() {
-                        if($(this).data('choice') === 'yes') {
-                            // RESULT: Gorgonzola
-                            showResult($q3, '<p>You are gorgonzola! A stinky boy!</p>', 'Gorgonzola');
+                        $('#q4 button').on('click', function() {
+                            if($(this).data('choice') === 'yes') {
+                                // RESULT: goat cheese
+                                showResult($q4, '<p>You are goat cheese! Bheeeee! </p>', 'GoatCheese');
 
-                        } else {
-                            // RESULT: Feta
-                            showResult($q3, '<p>You are feta! Strong and delicous!</p>', 'Feta');
-                        };
-                    })
-                    
-                } else {
-                    // goest to QUESTION 4 - Are you a crumbly mess?
-                    switchCard($q2, $q4);
+                            } else {
+                                // RESULT: brie
+                                showResult($q4,'<p>You are brie! Good with sweet AND savoury accompaniments! </p>', 'Brie');
+                            };
+                        });
+                    };
+                });
+            } else {
+                // goes to question 5
+                switchCard($q1, $q5);
 
-                    $('#q4 button').on('click', function() {
-                        if($(this).data('choice') === 'yes') {
-                            // RESULT: goat cheese
-                            showResult($q4, '<p>You are goat cheese! Bheeeee! </p>', 'GoatCheese');
+                $('#q5 button').on('click', function() {
+                    if ($(this).data('choice') === 'yes') {
+                        // RESULT: smoked gouda
+                        showResult($q5, '<p>You are smoked gouda! Gouda for you! </p>', 'Gouda');
 
-                        } else {
-                            // RESULT: brie
-                            showResult($q4,'<p>You are brie! Good with sweet AND savoury accompaniments! </p>', 'Brie');
-                        };
-                    });
-                };
-            });
-
-        } else {
-            // goes to question 5
-            switchCard($q1, $q5);
-
-            $('#q5 button').on('click', function() {
-                if ($(this).data('choice') === 'yes') {
-                    // RESULT: smoked gouda
-                    showResult($q5, '<p>You are smoked gouda! Gouda for you! </p>', 'Gouda');
-
-                } else {
-                    // RESULT: manchego
-                    showResult($q5,'<p>You are manchego! A tasty native of Spain!</p>', 'Manchego');
-                };
-            });
-        };
-        
+                    } else {
+                        // RESULT: manchego
+                        showResult($q5,'<p>You are manchego! A tasty native of Spain!</p>', 'Manchego');
+                    };
+                });
+            };
+        });
+        // End of quiz 
     });
-    // End of quiz 
-
-// reloads the page when the restart button at the end of the quiz is clicked on
-$restart.on('click', function() {
-    location.reload();
-})
 
 
-});
-
-
-
-
-
-
-
-
+    // reloads the page when the restart button at the end of the quiz is clicked on
+    $restart.on('click', function() {
+        location.reload();
+    });
 }); 
